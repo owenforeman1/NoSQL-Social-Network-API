d7fca609e56b62050a710f72ec3e62ea651db4d0
Owen Foreman <owen4man@gmail.com>
2022-05-08 15:59:14 -0400

    thought model

--- models/thought.js
+++ models/thought.js
@@ -1,1 +1,36 @@
-const mongoose = require("mongoose");
+const { Schema, model } = require("mongoose");
+const moment = require("moment");
+
+const thoughtSchema = new Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 280
+  },
+  createdAt: {
+    type: String,
+    default: Date.now,
+    get: createdAt => moment(createdAt).format("MMMM Do YYYY, h:mm:ss a")
+  },
+  username: 
+    {
+      type: String,
+      required: true,
+    },
+  reactions: [reactionSchema],
+    
+  toJson: {
+    virtuals: true,
+    getters: true
+  },
+  id: false,
+});
+
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model("Thought", thoughtSchema);
+
+module.exports = Thought;
