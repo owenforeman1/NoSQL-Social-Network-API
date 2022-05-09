2bbd10f4a8ce137f12d07fd2dad7ec4529a29406
Owen Foreman <owen4man@gmail.com>
2022-05-08 20:48:47 -0400

    user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,25 +1,87 @@
-const { Users } = require("../models");
+const res = require("express/lib/response");
+const { User } = require("../models");
 
-module.exports = {
-    getAllUsers(req, res) {
-      
-    },
+const userController = {
+  getAllUsers(req, res) {
+    User.find()
+      .then((users) => res.json(users))
+      .catch((err) => res.status(500).json(err));
+  },
   getUserById(req, res) {
-
+    User.findOne({ _id: req.params.userId })
+      .select("-__v")
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user found with that id" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
   },
   createNewUser(req, res) {
-
+    User.create(req.body)
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));
   },
   updateUserById(req, res) {
-
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user with this id" })
+          : res.json(user)
+      )
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
   },
   deleteUserById(req, res) {
-
+    User.findOneAndRemove({ _id: req.params.userId })
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user with this id" })
+          : User.findOneAndUpdate(
+              { users: req.params.userId },
+              { $pull: { users: req.params.userId } },
+              { new: true }
+            )
+      )
+      .then((user) =>
+        !user
+          ? res.status(404).json({
+              message: "User created but no user with this id",
+            })
+          : res.json({ message: "User successfully deleted" })
+      )
+      .catch((err) => res.status(500).json(err));
   },
   addFriend(req, res) {
-
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $addToSet: { tags: req.body } },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user with this id" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
   },
   removeFriend(req, res) {
-
-  }
-}
+      User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $pull: { tags: { tagId: req.params.tagId } } },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No user with this id' })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+};
